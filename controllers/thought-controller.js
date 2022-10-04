ea706e4203c9f445af464c06da1adc96c0646f8c
Jonathan Thompson <jathompson399@gmail.com>
2022-10-04 00:02:02 -0500

    final push before readme

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -93,7 +93,7 @@ const thoughtController = {
   deleteReaction({ params }, res) {
     Thought.findOneAndUpdate(
       { _id: params.thoughtId },
-      { $pull: { reaction: { reactionId: params.reactionId } } },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
       { new: true }
     )
       .then(dbThoughtData => res.json(dbThoughtData))
